e73ffda4ab099474cd8cfdec7519dd6db935f90e
Osawande <o_e16@txstate.edu>
2017-04-24 16:54:52 -0500

    Assignment 18 fail

--- src/test/java/testhello/TestGreeter.java
+++ src/test/java/testhello/TestGreeter.java
@@ -22,11 +22,29 @@ public class TestGreeter {
       assertEquals(g.getName(),"");
       assertEquals(g.sayHello(),"Hello!");
    }
+
+   @Test
+   public void newtestOKEGreeterPass()
+   {
+      g.setName("OKETest");
+      assertEquals(g.getName(), "OKETest");//Should succeed.
+   }
+
+   @Test
+   public void newtestOKEGreeterFail()
+   {
+      g.setName("failure");
+      assertEquals(g.getName(), "OKETest");//Should fail.
+   }
 	
    @Test
    public void testGreeter() 
    {
       g.setName("World");
+      assertEquals(g.getName(),"World");
+      assertEquals(g.sayHello(),"Hello World!");
+   }
+}
       assertEquals(g.getName(),"World");
       assertEquals(g.sayHello(),"Hello World!");
    }
